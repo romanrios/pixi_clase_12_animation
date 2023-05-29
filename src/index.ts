817724edf0c160e69500d364928a70e5e0f8399b
Roman <roromanrios@live.com>
2023-05-29 20:12:52 -0300

    movimos la imagen

--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,11 @@ const app = new Application({
 	height: 480
 });
 
-const clampy: Sprite = Sprite.from("clampy.png");
+const clampy: Sprite = Sprite.from("./clampy.png");
 
 clampy.anchor.set(0.5);
 
-clampy.x = app.screen.width / 2;
-clampy.y = app.screen.height / 2;
+clampy.x = 300;
+clampy.y = 300;
 
 app.stage.addChild(clampy);
